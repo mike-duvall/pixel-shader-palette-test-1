7b2dd4142a6e744dede216ac883765259ef3518c
mike-duvall <mike.a.duvall@gmail.com>
2019-11-09 08:41:23 -0600

    Initial commit of project

---
+++ pixel-shader-palette-test-1/pixel-shader-palette-test-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pixel_shader_palette_test_1
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
